67fe834b1609f58cdf228946c6302b48777686c8
Andréia Bohner <andreiabohner@gmail.com>
2025-08-05 20:54:17 -0300

    Update traits

--- src/Models/Traits/HasFavoriteForumPost.php
+++ src/Models/Traits/HasFavoriteForumPost.php
@@ -1,1 +1,14 @@
- 
+<?php
+
+namespace Tapp\FilamentForum\Models\Traits;
+
+use Tapp\FilamentForum\Models\ForumPost;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+trait HasFavoriteForumPost
+{
+    public function favoriteForumPosts(): BelongsToMany
+    {
+        return $this->belongsToMany(ForumPost::class, 'favorite_forum_post');
+    }
+}
