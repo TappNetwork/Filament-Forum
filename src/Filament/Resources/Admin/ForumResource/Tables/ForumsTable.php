e865584a3e1f264ebb6ac31c231d03021365c04b
Andréia Bohner <andreiabohner@gmail.com>
2025-10-01 22:33:18 -0300

    Missing namespace

--- src/Filament/Resources/Admin/ForumResource/Tables/ForumsTable.php
+++ src/Filament/Resources/Admin/ForumResource/Tables/ForumsTable.php
@@ -13,6 +13,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Enums\RecordActionsPosition;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class ForumsTable
 {
