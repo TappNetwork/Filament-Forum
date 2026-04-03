0474cd75834bf7d0f4a72c882f015a1b2735004b
scottgrayson <7796074+scottgrayson@users.noreply.github.com>
2026-04-03 17:20:58 +0000

    Fix styling

--- src/Filament/Resources/ForumPosts/Pages/ViewForumPost.php
+++ src/Filament/Resources/ForumPosts/Pages/ViewForumPost.php
@@ -2,10 +2,8 @@
 
 namespace Tapp\FilamentForum\Filament\Resources\ForumPosts\Pages;
 
-use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Support\Facades\Auth;
 use Tapp\FilamentForum\Concerns\HasCustomForumPostBreadcrumb;
 use Tapp\FilamentForum\Filament\Resources\ForumPosts\ForumPostResource;
 use Tapp\FilamentForum\Models\ForumPost;
